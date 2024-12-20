c580464e32032fce565a34187232d752f46a6447
Nipun Agarwal <nipunagarwal322@gmail.com>
2024-12-20 18:26:39 +0530

    get user data logic

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,6 +21,7 @@ app.use(
 
 // API Endpoints
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
