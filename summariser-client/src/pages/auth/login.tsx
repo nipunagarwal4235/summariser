eb16484dd788966acd3174736dbb015c7cfee2d7
Nipun Agarwal <nipunagarwal322@gmail.com>
2024-12-22 16:41:41 +0530

    fixed login sign up ui

--- summariser-client/src/pages/auth/login.tsx
+++ summariser-client/src/pages/auth/login.tsx
@@ -4,6 +4,17 @@ import { useNavigate } from "react-router";
 import axios from "axios";
 import { toast } from "react-toastify";
 
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function Login() {
   const navigate = useNavigate();
 
@@ -51,46 +62,89 @@ export default function Login() {
   };
 
   return (
-    <div>
-      <h2>{state === "Sign Up" ? "Create Account" : "Login"}</h2>
-      <p>{state === "Sign Up" ? "Create your Account" : "Login"}</p>
-      <form onSubmit={onSubmitHandler}>
-        {state === "Sign Up" && (
-          <input
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-            type="text"
-            placeholder="Full Name"
-            required
-          />
-        )}
-
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          type="email"
-          placeholder="Email"
-          required
-        />
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          type="password"
-          placeholder="Password"
-          required
-        />
-        <p onClick={() => navigate("/reset-password")}>Forgot Password</p>
-        <button type="submit">{state}</button>
-        {state === "Sign Up" ? (
-          <p onClick={() => setState("Login")}>
-            Already have an account. <span>Login Here</span>
-          </p>
-        ) : (
-          <p onClick={() => setState("Sign Up")}>
-            Don't have an account. <span>Sign Up Here</span>
-          </p>
-        )}
-      </form>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className="flex flex-col gap-6">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-2xl">
+                {state === "Sign Up" ? "Create Account" : "Login"}
+              </CardTitle>
+              <CardDescription>
+                {state === "Sign Up"
+                  ? "Enter your details to signup your account"
+                  : "Enter your details to login into your account"}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form onSubmit={onSubmitHandler}>
+                <div className="flex flex-col gap-6">
+                  <div className="grid gap-2">
+                    {state === "Sign Up" && (
+                      <>
+                        <Label htmlFor="name">Name</Label>
+                        <Input
+                          onChange={(e) => setName(e.target.value)}
+                          value={name}
+                          id="name"
+                          type="text"
+                          placeholder="John Doe"
+                          required
+                        />
+                      </>
+                    )}
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      onChange={(e) => setEmail(e.target.value)}
+                      value={email}
+                      id="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      required
+                    />
+                  </div>
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Password</Label>
+                    </div>
+                    <Input
+                      onChange={(e) => setPassword(e.target.value)}
+                      value={password}
+                      id="password"
+                      type="password"
+                      required
+                    />
+                    <p
+                      className="text-right underline cursor-pointer text-red-500"
+                      onClick={() => navigate("/reset-password")}
+                    >
+                      Forgot Password
+                    </p>
+                  </div>
+                  <Button type="submit" className="w-full">
+                    {state}
+                  </Button>
+                  {state === "Sign Up" ? (
+                    <p onClick={() => setState("Login")}>
+                      Already have an account.{" "}
+                      <span className="underline cursor-pointer text-blue-500 ">
+                        Login Here
+                      </span>
+                    </p>
+                  ) : (
+                    <p onClick={() => setState("Sign Up")}>
+                      Don't have an account.{" "}
+                      <span className="underline cursor-pointer text-blue-500 ">
+                        Sign Up Here
+                      </span>
+                    </p>
+                  )}
+                </div>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
